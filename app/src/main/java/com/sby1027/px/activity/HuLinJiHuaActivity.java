e80a8d11157cf31d8fbbc4b4fd8d93b9d4aa9645
Han <hanjianwei0917@163.com>
2017-08-07 14:07:54 +0800

    第二次提交

--- app/src/main/java/com/sby1027/px/activity/HuLinJiHuaActivity.java
+++ app/src/main/java/com/sby1027/px/activity/HuLinJiHuaActivity.java
@@ -1,8 +1,12 @@
 package com.sby1027.px.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.sby1027.px.HttpsActivity;
 import com.sby1027.px.R;
 
 public class HuLinJiHuaActivity extends AppCompatActivity {
@@ -11,5 +15,12 @@ public class HuLinJiHuaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hu_lin_ji_hua);
+        Button button = (Button) findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HuLinJiHuaActivity.this, HttpsActivity.class));
+            }
+        });
     }
 }
